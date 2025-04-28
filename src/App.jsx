c02dd6a67df1a5be5e72030247b7362056818ba9
DaryaGluschenko <darya.gluschenko@unity3d.com>
2025-04-28 21:16:48 +0300

    children prop

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { CORE_CONCEPTS } from './data';
 
 import Header from './components/Header/Header';
 import CoreConcept from './components/CoreConcept/CoreConcept';
+import TabButton from './components/TabButton';
 
 function App() {
   return (
@@ -17,6 +18,15 @@ function App() {
                 key = {index} />);
             })}
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
